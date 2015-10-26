8f9ba35abf2fa40012927dc33963659f57779570
unknown <Chaitanya.Pinnamanen@CARES-LT-044.CARES-DEV.Local>
2015-10-26 09:17:25 -0500

    Updated fragments to support fragments

--- app/src/main/java/com/eba/aislatech/FragmentUpdates.java
+++ app/src/main/java/com/eba/aislatech/FragmentUpdates.java
@@ -1,7 +1,7 @@
 package com.eba.aislatech;
 
 import android.app.ActivityOptions;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
